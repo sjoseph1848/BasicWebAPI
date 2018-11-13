288db6e36444ff3b74cc64d44b22bccdaab98bb5
Sam <sjoseph1848@gmail.com>
2018-11-13 07:26:04 -0600

    Added a new Controller for Products

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebServer.Models;
+
+[Route("ap/[controller]")]
+public class ProductsController : Controller
+{
+
+  [HttpGet]
+  public Product Action1([FromQuery]Product product)
+  {
+    return product;
+  }
+
+  [HttpGet("{id}/{name}/{price}")]
+  public Product Action2([FromRoute]Product product)
+  {
+    return product;
+  }
+
+  [HttpGet("{id}/{name}")]
+  public Product Action3(Product product)
+  {
+    return product;
+  }
+
+  [HttpPost]
+  public Product Action4([FromForm]Product product)
+  {
+    return product;
+  }
+
+  [HttpPost]
+  public Product Action5([FromBody]Product product)
+  {
+    return product;
+  }
+  [HttpPost("{id}/{name}/{price}")]
+  public Product[] Action6([FromRoute]Product p1, [FromQuery] Product p2, [FromBody]Product p3)
+  {
+    return new Product[] { p1, p2, p3 };
+  }
+  [HttpPost("{id}/{name}/{price}")]
+  public Product[] Action7([FromRoute]Product p1, [FromQuery] Product p2, [FromForm]Product p3)
+  {
+    return new Product[] { p1, p2, p3 };
+  }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,7 @@
+public class Product
+{
+  public int ID { get; set; }
+  public string Name { get; set; }
+
+  public double Price { get; set; }
+}
